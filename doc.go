e2bca5730fee06b53fa3a6906eac7cd8f1bcbaa7
Lonnie Liu <liulonnie@gmail.com>
2018-08-27 21:03:37 -0700

    .

---
+++ doc.go
@@ -0,0 +1,4 @@
+// Package yugong contains the code of a coding experiment. To
+// see if building small pieces of code can be used for creating a large
+// project.
+package yugong // import "shanhu.io/yugong"
